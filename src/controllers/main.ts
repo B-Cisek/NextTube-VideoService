43b5b0bd99871074e6c7d76b75162efaae7fd005
Bartłomiej Cisek <bartlomiej.cisek@outlook.com>
2024-07-20 00:07:58 +0200

    Schema, Validation

--- src/controllers/main.ts
+++ src/controllers/main.ts
@@ -1,10 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
 import { Controller } from '../decorators/controller';
 import { Route } from '../decorators/route';
+import Joi from 'joi';
+import { Validate } from '../decorators/validate';
+
+const postHealthCheckValidation = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email()
+});
 
 @Controller()
 export default class MainController {
     @Route('get', '/healthcheck')
+    @Route('post', '/healthcheck')
+    @Validate(postHealthCheckValidation)
     getHealthCheck(req: Request, res: Response, next: NextFunction) {
         logger.info('Health check called');
         return res.status(200).json({ hello: 'world!' });
